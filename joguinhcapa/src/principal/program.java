5801a68c8319137feaa0c62fd73d617285ecfdcf
Gustavo <Gustavo@DESKTOP-K3J97NH>
2024-11-28 19:47:07 -0300

    teste23

---
+++ joguinhcapa/src/principal/program.java
@@ -0,0 +1,16 @@
+package principal;
+
+import java.util.Locale;
+
+public class program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		System.out.println("teste");
+		
+		
+		
+		
+	}
+
+}
