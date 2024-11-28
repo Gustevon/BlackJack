e8c70694dbaf1fe3cfce1176ccf650b74bfce397
Gustavo <Gustavo@DESKTOP-K3J97NH>
2024-11-28 20:21:45 -0300

    321

--- joguinhcapa/src/principal/baralho.java
+++ joguinhcapa/src/principal/baralho.java
@@ -1,5 +1,33 @@
 package principal;
 
-public class baralho {
+import java.util.ArrayList;
+import java.util.Collections;
 
+public class baralho {
+    private ArrayList<carta> cartas;
+
+    public baralho() {
+        cartas = new ArrayList<>();
+        String[] valores = {"Ás", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Valete", "Dama", "Rei"};
+        String[] naipes = {"Copas", "Ouros", "Espadas", "Paus"};
+        int[] pontuacoes = {11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10};
+
+        for (String naipe : naipes) {
+            for (int i = 0; i < valores.length; i++) {
+                cartas.add(new carta(valores[i], naipe, pontuacoes[i]));
+            }
+        }
+
+        embaralhar();
+    }
+
+    public void embaralhar() {
+        Collections.shuffle(cartas);
+    }
+
+    public carta distribuir() {
+        return cartas.remove(0);
+    }
 }
+
+
--- joguinhcapa/src/principal/carta.java
+++ joguinhcapa/src/principal/carta.java
@@ -2,8 +2,35 @@ package principal;
 
 public class carta {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+
+		    private String valor;
+		    private String naipe;
+		    private int pontuacao;
+
+		    public carta(String valor, String naipe, int pontuacao) {
+		        this.valor = valor;
+		        this.naipe = naipe;
+		        this.pontuacao = pontuacao;
+		    }
+
+		    public String getValor() {
+		        return valor;
+		    }
+
+		    public String getNaipe() {
+		        return naipe;
+		    }
+
+		    public int getPontuacao() {
+		        return pontuacao;
+		    }
+
+		    @Override
+		    public String toString() {
+		        return valor + " de " + naipe;
+		    
+		
 
 	}
 
